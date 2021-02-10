c8e5abc885c09f93ed2b7d9eea4c44d44ef910e8
sahadip28 <54122309+sahadip28@users.noreply.github.com>
2021-02-09 21:41:28 -0600

    Update createpage.java

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.14.28.141:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://10.128.0.4:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
